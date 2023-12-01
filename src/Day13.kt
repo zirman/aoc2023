932eb82064dcb0dc983518a0ecc3715020164404
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-01 12:14:42 -0500

    Added scaffold for all days.

---
+++ src/Day13.kt
@@ -0,0 +1,20 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        TODO()
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput1 = readInput("Day13_1_test")
+    check(part1(testInput1) == TODO())
+
+//    val testInput2 = readInput("Day13_2_test")
+//    check(part2(testInput2) == 1)
+
+    val input = readInput("Day13")
+    part1(input).println()
+//    part2(input).println()
+}
