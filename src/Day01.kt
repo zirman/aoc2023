9c19ffd315ea1dc1d073e2f23a8a5da8ea9e3770
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-01 09:21:48 -0500

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,52 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            "${line.find { it.isDigit() }}${line.findLast { it.isDigit() }}".toInt()
+        }
     }
+
+    val numbers = mapOf(
+        "1" to 1,
+        "one" to 1,
+        "2" to 2,
+        "two" to 2,
+        "3" to 3,
+        "three" to 3,
+        "4" to 4,
+        "four" to 4,
+        "5" to 5,
+        "five" to 5,
+        "6" to 6,
+        "six" to 6,
+        "7" to 7,
+        "seven" to 7,
+        "8" to 8,
+        "eight" to 8,
+        "9" to 9,
+        "nine" to 9,
+    ).mapKeys { (k) -> k.toRegex() }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        fun String.checkMatch(i: Int): Int? {
+            return numbers.keys
+                .find { regex -> regex.matchesAt(this, i) }
+                ?.let { numbers[it] }
+        }
+
+        return input.sumOf { line ->
+            "${
+                line.indices.firstNotNullOf { i -> line.checkMatch(i) }
+            }${
+                line.indices.reversed().firstNotNullOf { i -> line.checkMatch(i) }
+            }".toInt()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01_1_test")
+    check(part1(testInput1) == 142)
+
+    val testInput2 = readInput("Day01_2_test")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
