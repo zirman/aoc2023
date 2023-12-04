056db6a35c14e72e9f3a58ff32be2ca52d146f46
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-04 10:09:56 -0500

    Day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,27 +2,25 @@ fun main() {
     fun part1(input: List<String>): Int {
         val wsRegex = """\s+""".toRegex()
 
-        input
-            .sumOf { line ->
-                val (_, combinedNumbers) = line.split(":")
-                val (winningNumbersStr, numbersStr) = combinedNumbers.split("|")
-                val winningNumbers = winningNumbersStr.trim().split(wsRegex).toSet()
-                val numbers = numbersStr.trim().split(wsRegex)
-                1.shl(numbers.count { winningNumbers.contains(it) } - 1)
-            }
-            .run { return this }
+        return input.sumOf { line ->
+            val (_, combinedNumbers) = line.split(":")
+            val (winningNumbersStr, numbersStr) = combinedNumbers.split("|")
+            val winningNumbers = winningNumbersStr.trim().split(wsRegex).toSet()
+            val numbers = numbersStr.trim().split(wsRegex)
+            1.shl(numbers.count { winningNumbers.contains(it) } - 1)
+        }
     }
 
     fun part2(input: List<String>): Int {
         val wsRegex = """\s+""".toRegex()
-        val cardPoints = input
-            .map { line ->
-                val (_, combinedNumbers) = line.split(":")
-                val (winningNumbersStr, numbersStr) = combinedNumbers.split("|")
-                val winningNumbers = winningNumbersStr.trim().split(wsRegex).map { it.toInt() }.toSet()
-                val numbers = numbersStr.trim().split(wsRegex).map { it.toInt() }
-                numbers.count { winningNumbers.contains(it) }
-            }
+
+        val cardPoints = input.map { line ->
+            val (_, combinedNumbers) = line.split(":")
+            val (winningNumbersStr, numbersStr) = combinedNumbers.split("|")
+            val winningNumbers = winningNumbersStr.trim().split(wsRegex).map { it.toInt() }.toSet()
+            val numbers = numbersStr.trim().split(wsRegex).map { it.toInt() }
+            numbers.count { winningNumbers.contains(it) }
+        }
 
         fun winningScratchCardCount(i: Int): Int {
             val memo = mutableMapOf<Int, Int>()
