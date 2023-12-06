3ff8c8fc576c9098c2152b64d752df40db2b7155
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-06 10:03:17 -0500

    Day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,20 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        TODO()
+        val times = input[0].substringAfter("Time:").trim().split("""\s+""".toRegex()).map { it.toInt() }
+        val distances = input[1].substringAfter("Distance:").trim().split("""\s+""".toRegex()).map { it.toInt() }
+        return times
+            .zip(distances)
+            .map { (time, distance) ->
+                (1..<time)
+                    .map { (time - it) * it }
+                    .count { it > distance }
+            }
+            .reduce(Int::times)
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val time = input[0].substringAfter("Time:").trim().filter { it.isDigit() }.toLong()
+        val distance = input[1].substringAfter("Distance:").trim().filter { it.isDigit() }.toLong()
+        return (1..<time)
+            .map { (time - it) * it }
+            .count { it > distance }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput1 = readLines("Day06_1_test")
-    check(part1(testInput1) == TODO())
-
-//    val testInput2 = readInput("Day06_2_test")
-//    check(part2(testInput2) == 1)
+    check(part1(testInput1) == 288)
+    check(part2(testInput1) == 71503)
 
     val input = readLines("Day06")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
