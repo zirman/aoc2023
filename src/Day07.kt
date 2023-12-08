066d4e0b9987387c89aa0a02fb628761e2992587
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-08 11:04:12 -0500

    Day 8

--- src/Day07.kt
+++ src/Day07.kt
@@ -9,17 +9,20 @@ fun main() {
         val hands = input.map { it.split(' ') }
         hands
             .sortedWith(compareBy<List<String>> { (cards, _) ->
-                val counts =
-                    cards.toList().groupingBy { it }.eachCount().toList().sortedByDescending { (_, count) -> count }
-                when (counts[0].second) {
+                val kindCounts = cards
+                    .toList().groupingBy { it }
+                    .eachCount().toList()
+                    .sortedByDescending { (_, count) -> count }
+                    .map { (_, count) -> count }
+                when (kindCounts[0]) {
                     5 -> 6 // 5 of a kind
                     4 -> 5 // 4 of a kind
-                    3 -> when (counts[1].second) {
+                    3 -> when (kindCounts[1]) {
                         2 -> 4 // full house
                         else -> 3 // three of a kind
                     }
 
-                    2 -> when (counts[1].second) {
+                    2 -> when (kindCounts[1]) {
                         2 -> 2 // two pair
                         else -> 1  // one pair
                     }
@@ -28,24 +31,8 @@ fun main() {
                     else -> never()
                 }
             }.thenBy { (cards, _) ->
-                cards.fold(0) { order, card ->
-                    order * 13 + when (card) {
-                        'A' -> 12
-                        'K' -> 11
-                        'Q' -> 10
-                        'J' -> 9
-                        'T' -> 8
-                        '9' -> 7
-                        '8' -> 6
-                        '7' -> 5
-                        '6' -> 4
-                        '5' -> 3
-                        '4' -> 2
-                        '3' -> 1
-                        '2' -> 0
-                        else -> never()
-                    }
-                }
+                val cardMapping = "23456789TJQKA"
+                cards.fold(0) { order, card -> (order * cardMapping.length) + cardMapping.indexOf(card) }
             })
             .mapIndexed { index, (_, bid) -> (index + 1) * bid.toLong() }
             .sum()
@@ -60,16 +47,17 @@ fun main() {
                 val (wildcardsList, nonWildcardsList) = cardCountsList.partition { (card, _) -> card == 'J' }
                 val nonWildcardsSortedList = nonWildcardsList.sortedByDescending { (_, count) -> count }
                 val wildcardCount = wildcardsList.sumOf { (_, count) -> count }
-                val mostCommonCardCount = nonWildcardsSortedList.getOrNull(0)?.second ?: 0
-                when (mostCommonCardCount + wildcardCount) {
+                val kindCounts = nonWildcardsSortedList.map { (_, count) -> count }.ifEmpty { listOf(0) }
+                    .mapIndexed { index, i -> if (index == 0) i + wildcardCount else i }
+                when (kindCounts[0]) {
                     5 -> 6 // 5 of a kind
                     4 -> 5 // 4 of a kind
-                    3 -> when (nonWildcardsSortedList[1].second) {
+                    3 -> when (kindCounts[1]) {
                         2 -> 4 // full house
                         else -> 3 // three of a kind
                     }
 
-                    2 -> when (nonWildcardsSortedList[1].second) {
+                    2 -> when (kindCounts[1]) {
                         2 -> 2 // two pair
                         else -> 1  // one pair
                     }
@@ -78,24 +66,8 @@ fun main() {
                     else -> never()
                 }
             }.thenBy { (cards, _) ->
-                cards.fold(0) { order, card ->
-                    order * 13 + when (card) {
-                        'A' -> 12
-                        'K' -> 11
-                        'Q' -> 10
-                        'T' -> 9
-                        '9' -> 8
-                        '8' -> 7
-                        '7' -> 6
-                        '6' -> 5
-                        '5' -> 4
-                        '4' -> 3
-                        '3' -> 2
-                        '2' -> 1
-                        'J' -> 0
-                        else -> never()
-                    }
-                }
+                val cardMapping = "J23456789TQKA"
+                cards.fold(0) { order, card -> (order * cardMapping.length) + cardMapping.indexOf(card) }
             })
             .mapIndexed { index, (_, bid) -> (index + 1) * bid.toLong() }
             .sum()
--- src/Day08.kt
+++ src/Day08.kt
@@ -1,20 +1,83 @@
+import kotlin.system.measureTimeMillis
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun part1(input: String): Int {
+        val regex = """(\w\w\w) = \((\w\w\w), (\w\w\w)\)""".toRegex()
+        val (instructionsString, networkStrings) = input.split("\n\n")
+
+        val map = networkStrings.split('\n').filter { it.isNotEmpty() }.associate { line ->
+            val (node, leftNode, rightNode) = regex.matchEntire(line)!!.destructured
+            Pair(node, Pair(leftNode, rightNode))
+        }
+
+        val instructionsList = instructionsString.toList()
+        sequence { while (true) yieldAll(instructionsList) }
+            .scan("AAA") { location, direction ->
+                when (direction) {
+                    'L' -> map[location]!!.first
+                    'R' -> map[location]!!.second
+                    else -> never()
+                }
+            }
+            .takeWhile { location -> location != "ZZZ" }
+            .count()
+            .run { return this }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: String): Long {
+        val regex = """(\w\w\w) = \((\w\w\w), (\w\w\w)\)""".toRegex()
+        val (instructionsString, networkStrings) = input.split("\n\n")
+
+        val map = networkStrings.split('\n').filter { it.isNotEmpty() }.associate { line ->
+            val (node, leftNode, rightNode) = regex.matchEntire(line)!!.destructured
+            Pair(node, Pair(leftNode, rightNode))
+        }
+
+        val instructionsList = instructionsString.toList()
+
+        fun search(startLocation: String): List<Long> {
+            val visited = mutableSetOf<String>()
+            val endings = mutableListOf<Long>()
+            var depth = 0L
+            var location = startLocation
+            val directionIterator = sequence { while (true) yieldAll(instructionsList) }.iterator()
+
+            while (true) {
+                if (visited.contains(location)) {
+                    return endings
+                }
+
+                if (location.endsWith('Z')) {
+                    endings.add(depth)
+                    visited.add(location)
+                }
+
+                location = when (directionIterator.next()) {
+                    'L' -> map[location]!!.first
+                    'R' -> map[location]!!.second
+                    else -> never()
+                }
+
+                depth++
+            }
+        }
+
+        val x = map.keys
+            .filter { it.endsWith('A') }
+            .map { search(it).last().toLong() }
+        val xmax = x.max()
+        return (1L..Long.MAX_VALUE).first {
+            val q = it * xmax
+            x.all { q % it == 0L }
+        } * xmax
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput1 = readLines("Day08_1_test")
-    check(part1(testInput1) == TODO())
+    val testInput1 = readFile("Day08_1_test")
+    check(part1(testInput1) == 6)
+    val testInput2 = readFile("Day08_2_test")
+    check(part2(testInput2) == 6L)
 
-//    val testInput2 = readInput("Day08_2_test")
-//    check(part2(testInput2) == 1)
-
-    val input = readLines("Day08")
-    part1(input).println()
-//    part2(input).println()
+    val input = readFile("Day08")
+    measureTimeMillis { part1(input).println() }.also { println("time: $it") }
+    measureTimeMillis { part2(input).println() }.also { println("time: $it") }
 }
