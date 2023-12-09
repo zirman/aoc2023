f74d8ddf4b90213be370267feeec24fb1b3d17f5
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-09 09:10:38 -0500

    Day 9

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,20 +1,44 @@
 fun main() {
+    fun List<Int>.recur(): List<List<Int>> {
+        return if (all { it == 0 }) listOf(this)
+        else buildList {
+            add(this@recur)
+            this@recur
+                .windowed(2, 1).map { (previousNumber, nextNumber) ->
+                    nextNumber - previousNumber
+                }
+                .recur()
+                .run { addAll(this) }
+        }
+    }
+
+    fun String.parse(): List<Int> {
+        return split(' ').map { it.toInt() }
+    }
+
     fun part1(input: List<String>): Int {
-        TODO()
+        input
+            .sumOf { line -> line.parse().recur().asReversed().sumOf { numbers -> numbers.last() } }
+            .run { return this }
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        input
+            .sumOf { line ->
+                line.parse()
+                    .recur()
+                    .asReversed()
+                    .map { it.first() }
+                    .reduce { acc, n -> n - acc }
+            }
+            .run { return this }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput1 = readLines("Day09_1_test")
-    check(part1(testInput1) == TODO())
-
-//    val testInput2 = readInput("Day09_2_test")
-//    check(part2(testInput2) == 1)
+    check(part1(testInput1) == 114)
+    check(part2(testInput1) == 2)
 
     val input = readLines("Day09")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
