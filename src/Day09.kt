6ee1d34e1b453c5c83a2d45361648eaaa69b8c09
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-09 12:14:44 -0500

    Day 9 code golfed

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,40 +1,18 @@
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    fun List<Int>.recur(): List<List<Int>> {
-        return if (all { it == 0 }) listOf(this)
-        else buildList {
-            add(this@recur)
-            this@recur
-                .windowed(2, 1).map { (previousNumber, nextNumber) ->
-                    nextNumber - previousNumber
-                }
-                .recur()
-                .run { addAll(this) }
-        }
+    fun String.parse(): List<Int> = split(' ').map { it.toInt() }
+
+    fun recur(nums: List<Int>): List<List<Int>> = buildList {
+        if (nums.any { it != 0 }) addAll(recur(nums.windowed(2, 1).map { (p, n) -> n - p }))
+        add(nums)
     }
 
-    fun String.parse(): List<Int> {
-        return split(' ').map { it.toInt() }
-    }
+    fun part1(input: List<String>): Int =
+        input.sumOf { line -> recur(line.parse()).sumOf { numbers -> numbers.last() } }
 
-    fun part1(input: List<String>): Int {
-        input
-            .sumOf { line -> line.parse().recur().asReversed().sumOf { numbers -> numbers.last() } }
-            .run { return this }
-    }
-
-    fun part2(input: List<String>): Int {
-        input
-            .sumOf { line ->
-                line.parse()
-                    .recur()
-                    .asReversed()
-                    .map { it.first() }
-                    .reduce { acc, n -> n - acc }
-            }
-            .run { return this }
-    }
+    fun part2(input: List<String>): Int =
+        input.sumOf { line -> recur(line.parse()).map { it.first() }.reduce { acc, n -> n - acc } }
 
     val testInput1 = readLines("Day09_1_test")
     check(part1(testInput1) == 114)
