82db1eb8cd9f103ccf844c07179f88545e1be611
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-10 12:03:45 -0500

    Day 10 cleanup

--- src/Day10.kt
+++ src/Day10.kt
@@ -3,188 +3,173 @@ import kotlinx.collections.immutable.persistentSetOf
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    fun part1(input: List<String>): Int {
+    operator fun List<List<Char>>.get(rowIndex: Int, columnIndex: Int): Char? =
+        getOrNull(rowIndex)?.getOrNull(columnIndex)
+
+    fun Char.connectsNorth(): Boolean = "|JL".contains(this)
+    fun Char.connectsSouth(): Boolean = "|7F".contains(this)
+    fun Char.connectsWest(): Boolean = "-7J".contains(this)
+    fun Char.connectsEast(): Boolean = "-LF".contains(this)
+
+    fun parse(input: List<String>): Triple<List<List<Char>>, Int, Int> {
         val pipes = input.map { it.toList() }
         val startRowIndex = pipes.indexOfFirst { it.contains('S') }
         val startColumnIndex = pipes[startRowIndex].indexOfFirst { it == 'S' }
 
-        fun connectedPipes(rowIndex: Int, columnIndex: Int): List<Pair<Int, Int>> = buildList {
-            if (pipes.getOrNull(rowIndex - 1)?.get(columnIndex)?.let { "|F7".contains(it) } == true) {
-                add(Pair(rowIndex - 1, columnIndex))
-            }
-            if (pipes.getOrNull(rowIndex + 1)?.get(columnIndex)?.let { "|LJ".contains(it) } == true) {
-                add(Pair(rowIndex + 1, columnIndex))
-            }
-            if (pipes[rowIndex].getOrNull(columnIndex - 1)?.let { "-LF".contains(it) } == true) {
-                add(Pair(rowIndex, columnIndex - 1))
-            }
-            if (pipes[rowIndex].getOrNull(columnIndex + 1)?.let { "-7J".contains(it) } == true) {
-                add(Pair(rowIndex, columnIndex + 1))
-            }
+        return Triple(
+            pipes.map { row ->
+                row.map {
+                    if (it == 'S') {
+                        when {
+                            pipes[startRowIndex - 1, startColumnIndex]?.connectsSouth() == true -> when {
+                                pipes[startRowIndex + 1, startColumnIndex]?.connectsNorth() == true -> '|'
+                                pipes[startRowIndex, startColumnIndex - 1]?.connectsWest() == true -> 'J'
+                                pipes[startRowIndex, startColumnIndex + 1]?.connectsEast() == true -> 'L'
+                                else -> never()
+                            }
+
+                            pipes[startRowIndex, startColumnIndex - 1]?.connectsEast() == true -> when {
+                                pipes[startRowIndex, startColumnIndex + 1]?.connectsWest() == true -> '-'
+                                pipes[startRowIndex + 1, startColumnIndex]?.connectsNorth() == true -> '7'
+                                else -> never()
+                            }
+
+                            pipes[startRowIndex, startColumnIndex + 1]?.connectsWest() == true -> when {
+                                pipes[startRowIndex + 1, startColumnIndex]?.connectsNorth() == true -> 'F'
+                                else -> never()
+                            }
+
+                            else -> never()
+                        }
+                    } else {
+                        it
+                    }
+                }
+            },
+            startRowIndex,
+            startColumnIndex
+        )
+    }
+
+    fun List<List<Char>>.connectedPipes(rowIndex: Int, columnIndex: Int): List<Pair<Int, Int>> = buildList {
+        val pipes = this@connectedPipes
+        if (pipes[rowIndex][columnIndex].connectsNorth() &&
+            pipes[rowIndex - 1, columnIndex]?.connectsSouth() == true
+        ) {
+            add(Pair(rowIndex - 1, columnIndex))
         }
+        if (pipes[rowIndex][columnIndex].connectsSouth() &&
+            pipes[rowIndex + 1, columnIndex]?.connectsNorth() == true
+        ) {
+            add(Pair(rowIndex + 1, columnIndex))
+        }
+        if (pipes[rowIndex][columnIndex].connectsWest() &&
+            pipes[rowIndex, columnIndex - 1]?.connectsEast() == true
+        ) {
+            add(Pair(rowIndex, columnIndex - 1))
+        }
+        if (pipes[rowIndex][columnIndex].connectsEast() &&
+            pipes[rowIndex, columnIndex + 1]?.connectsWest() == true
+        ) {
+            add(Pair(rowIndex, columnIndex + 1))
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        val (pipes, startRowIndex, startColumnIndex) = parse(input)
 
         tailrec fun breathFirstSearch(
             depth: Int,
             current: List<Pair<Int, Int>>,
             visited: PersistentSet<Pair<Int, Int>>,
         ): Int {
-            if (current.isEmpty()) return depth
             val next = current
-                .flatMap { (rowIndex, columnIndex) -> connectedPipes(rowIndex, columnIndex) }
+                .flatMap { (rowIndex, columnIndex) -> pipes.connectedPipes(rowIndex, columnIndex) }
                 .filter { visited.contains(it).not() }
-            return breathFirstSearch(depth + 1, next, visited.addAll(current))
+
+            return if (next.isEmpty()) depth else breathFirstSearch(depth + 1, next, visited.addAll(current))
         }
 
         return breathFirstSearch(
             depth = 0,
             current = listOf(Pair(startRowIndex, startColumnIndex)),
-            visited = persistentSetOf()
-        ) - 1
+            visited = persistentSetOf(),
+        )
     }
 
     fun part2(input: List<String>): Int {
-        var pipes = input.map { it.toList() }
-        val startRowIndex = pipes.indexOfFirst { it.contains('S') }
-        val startColumnIndex = pipes[startRowIndex].indexOfFirst { it == 'S' }
-
-        val startPipe =
-            if (pipes.getOrNull(startRowIndex - 1)?.get(startColumnIndex)?.let { "|7F".contains(it) } == true) {
-                if (pipes.getOrNull(startRowIndex + 1)?.get(startColumnIndex)?.let { "|JL".contains(it) } == true) {
-                    '|'
-                } else if (pipes[startRowIndex].getOrNull(startColumnIndex - 1)?.let { "-FL".contains(it) } == true) {
-                    'J'
-                } else if (pipes[startRowIndex].getOrNull(startColumnIndex + 1)?.let { "-FL".contains(it) } == true) {
-                    'L'
-                } else {
-                    never()
-                }
-            } else if (pipes[startRowIndex].getOrNull(startColumnIndex - 1)?.let { "-FL".contains(it) } == true) {
-                if (pipes[startRowIndex].getOrNull(startColumnIndex + 1)?.let { "-7J".contains(it) } == true) {
-                    '-'
-                } else if (pipes.getOrNull(startRowIndex + 1)?.get(startColumnIndex)
-                        ?.let { "|LJ".contains(it) } == true
-                ) {
-                    '7'
-                } else {
-                    never()
-                }
-            } else if (pipes[startRowIndex].getOrNull(startColumnIndex + 1)?.let { "-7J".contains(it) } == true) {
-                if (pipes.getOrNull(startRowIndex + 1)?.get(startColumnIndex)?.let { "|JL".contains(it) } == true) {
-                    'F'
-                } else {
-                    never()
-                }
-            } else {
-                never()
-            }
-
-        pipes = pipes.map { l -> l.map { if (it == 'S') startPipe else it } }
-
-        fun connectedPipes(rowIndex: Int, columnIndex: Int): List<Pair<Int, Int>> = buildList {
-            if ("|LJ".contains(pipes[rowIndex][columnIndex]) &&
-                pipes.getOrNull(rowIndex - 1)?.get(columnIndex)?.let { "|F7".contains(it) } == true
-            ) {
-                add(Pair(rowIndex - 1, columnIndex))
-            }
-            if ("|F7".contains(pipes[rowIndex][columnIndex]) &&
-                pipes.getOrNull(rowIndex + 1)?.get(columnIndex)?.let { "|LJ".contains(it) } == true
-            ) {
-                add(Pair(rowIndex + 1, columnIndex))
-            }
-            if ("-J7".contains(pipes[rowIndex][columnIndex]) &&
-                pipes[rowIndex].getOrNull(columnIndex - 1)?.let { "-LF".contains(it) } == true
-            ) {
-                add(Pair(rowIndex, columnIndex - 1))
-            }
-            if ("-FL".contains(pipes[rowIndex][columnIndex]) &&
-                pipes[rowIndex].getOrNull(columnIndex + 1)?.let { "-7J".contains(it) } == true
-            ) {
-                add(Pair(rowIndex, columnIndex + 1))
-            }
-        }
+        val (pipes, startRowIndex, startColumnIndex) = parse(input)
 
         tailrec fun breathFirstSearch(
             depth: Int,
             current: List<Pair<Int, Int>>,
             visited: PersistentSet<Pair<Int, Int>>,
         ): PersistentSet<Pair<Int, Int>> {
-            if (current.isEmpty()) return visited
             val next = current
-                .flatMap { (rowIndex, columnIndex) -> connectedPipes(rowIndex, columnIndex) }
+                .flatMap { (rowIndex, columnIndex) -> pipes.connectedPipes(rowIndex, columnIndex) }
                 .filter { visited.contains(it).not() }
-            return breathFirstSearch(depth + 1, next, visited.addAll(current))
-        }
 
-        val visited = breathFirstSearch(
-            depth = 0,
-            current = listOf(Pair(startRowIndex, startColumnIndex)),
-            visited = persistentSetOf()
-        )
-
-        println(visited)
-
-        val culledPipes = pipes.mapIndexed { rowIndex, row ->
-            row.mapIndexed { columnIndex, pipe ->
-                if (startRowIndex == rowIndex && startColumnIndex == columnIndex) {
-                    startPipe
-                } else if (visited.contains(Pair(rowIndex, columnIndex))) {
-                    pipe
-                } else {
-                    '.'
-                }
+            return if (next.isEmpty()) {
+                visited.addAll(current)
+            } else {
+                breathFirstSearch(depth = depth + 1, current = next, visited = visited.addAll(current))
             }
         }
 
-        culledPipes.joinToString("\n") { row -> row.joinToString("") }.also { println(it) }
-        println()
+        val visitedPipes = breathFirstSearch(
+            depth = 0,
+            current = listOf(Pair(startRowIndex, startColumnIndex)),
+            visited = persistentSetOf(),
+        )
 
-        val filledPipes = culledPipes
+        pipes
+            .mapIndexed { rowIndex, row ->
+                row.mapIndexed { columnIndex, pipe ->
+                    if (visitedPipes.contains(Pair(rowIndex, columnIndex))) {
+                        pipe
+                    } else {
+                        '.'
+                    }
+                }
+            }
             .map { row ->
                 var crossings = 0
                 var cornerCrossing = '.'
+
                 row.map { pipe ->
                     when (pipe) {
-                        '.' -> {
-                            if (crossings % 2 == 0) 'O' else '*'
-                        }
+                        '.' -> if (crossings % 2 == 0) 'O' else 'I'
 
                         '|' -> {
                             crossings++
                             pipe
                         }
 
+                        // start possible corner crossing
                         'F', 'L' -> {
                             cornerCrossing = pipe
                             pipe
                         }
 
                         'J' -> {
-                            if (cornerCrossing == 'F') {
-                                crossings++
-                                cornerCrossing = '.'
-                            }
+                            // finish corner crossing
+                            if (cornerCrossing == 'F') crossings++
                             pipe
                         }
 
                         '7' -> {
-                            if (cornerCrossing == 'L') {
-                                crossings++
-                                cornerCrossing = '.'
-                            }
+                            // finish corner crossing
+                            if (cornerCrossing == 'L') crossings++
                             pipe
                         }
 
-                        '-' -> {
-                            pipe
-                        }
-
+                        '-' -> pipe
                         else -> never()
                     }
                 }
             }
-
-        filledPipes.joinToString("\n") { row -> row.joinToString("") }.also { println(it) }
-        return filledPipes.sumOf { row -> row.count { it == '*' } }
+            .sumOf { row -> row.count { it == 'I' } }
+            .run { return this }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -192,7 +177,7 @@ fun main() {
     check(part1(testInput1) == 8)
 
     val testInput2 = readLines("Day10_2_test")
-    check(part2(testInput2).also { println(it) } == 10)
+    check(part2(testInput2) == 10)
 
     val input = readLines("Day10")
     measureTimeMillis { part1(input).println() }.also { println("time: $it") }
