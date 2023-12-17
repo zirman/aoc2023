8c0d3009bc41c397abcba5f0b49cd6fd3769277d
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2023-12-17 17:54:57 -0500

    Day 17 Cleanup

--- src/Day17.kt
+++ src/Day17.kt
@@ -10,18 +10,24 @@ data class Crucible(
 )
 
 fun <T> aStar(
-    startNodes: Set<T>,
-    comparator: Comparator<T>,
+    startNodes: Set<Pair<T, Long>>,
     isEnd: T.() -> Boolean,
-    nextNodes: T.() -> Set<T>,
-): T {
-    val queue = PriorityQueue(comparator)
+    nextNodes: Pair<T, Long>.() -> Set<Pair<T, Long>>,
+): Pair<T, Long> {
+    val visited = startNodes.associate { it }.toMutableMap()
+    val queue = PriorityQueue(compareBy<Pair<T, Long>> { (_, cost) -> cost })
     queue.addAll(startNodes)
 
-    tailrec fun recur(): T {
-        val next = queue.poll()!!
-        if (next.isEnd()) return next
-        queue.addAll(next.nextNodes().filter { queue.contains(it).not() })
+    tailrec fun recur(): Pair<T, Long> {
+        val current = queue.poll()!!
+        if (current.first.isEnd()) return current
+        current
+            .nextNodes()
+            .filter { it.second < visited.getOrDefault(it.first, Long.MAX_VALUE) && queue.contains(it).not() }
+            .run {
+                queue.addAll(this)
+                visited.putAll(this)
+            }
         return recur()
     }
 
@@ -31,96 +37,95 @@ fun <T> aStar(
 fun main() {
     fun part1(input: List<String>): Long {
         val map = input.map { line -> line.map { it.code - '0'.code } }
-        val visited = mutableMapOf<Crucible, Long>()
 
-        fun Crucible.nextStart(cooled: Long): Set<Pair<Crucible, Long>> = buildSet {
-            fun goNorth(stepsTaken: Int) {
-                if (rowIndex > 0) {
-                    val k = copy(
-                        rowIndex = rowIndex - 1,
-                        direction = North,
-                        stepsTaken = stepsTaken,
-                    )
-                    val v = cooled + map[rowIndex - 1][columnIndex]
-                    val q = visited[k]
-                    if (q == null || q > v) {
-                        visited[k] = v
-                        add(Pair(k, v))
+        fun Pair<Crucible, Long>.nextStart(): Set<Pair<Crucible, Long>> = buildSet {
+            val (crucible, cooled) = this@nextStart
+
+            with(crucible) {
+                fun goNorth(stepsTaken: Int) {
+                    if (rowIndex > 0) {
+                        add(
+                            Pair(
+                                copy(
+                                    rowIndex = rowIndex - 1,
+                                    direction = North,
+                                    stepsTaken = stepsTaken,
+                                ),
+                                cooled + map[rowIndex - 1][columnIndex],
+                            )
+                        )
                     }
                 }
-            }
 
-            fun goSouth(stepsTaken: Int) {
-                if (rowIndex < map.size - 1) {
-                    val c = cooled + map[rowIndex + 1][columnIndex]
-                    val k = copy(
-                        rowIndex = rowIndex + 1,
-                        direction = South,
-                        stepsTaken = stepsTaken,
-                    )
-                    val q = visited[k]
-                    if (q == null || q > c) {
-                        visited[k] = c
-                        add(Pair(k, c))
+                fun goSouth(stepsTaken: Int) {
+                    if (rowIndex < map.size - 1) {
+                        add(
+                            Pair(
+                                copy(
+                                    rowIndex = rowIndex + 1,
+                                    direction = South,
+                                    stepsTaken = stepsTaken,
+                                ),
+                                cooled + map[rowIndex + 1][columnIndex],
+                            )
+                        )
                     }
                 }
-            }
 
-            fun goEast(stepsTaken: Int) {
-                if (columnIndex < map[0].size - 1) {
-                    val c = cooled + map[rowIndex][columnIndex + 1]
-                    val k = copy(
-                        columnIndex = columnIndex + 1,
-                        direction = East,
-                        stepsTaken = stepsTaken,
-                    )
-                    val q = visited[k]
-                    if (q == null || q > c) {
-                        visited[k] = c
-                        add(Pair(k, c))
+                fun goEast(stepsTaken: Int) {
+                    if (columnIndex < map[0].size - 1) {
+                        add(
+                            Pair(
+                                copy(
+                                    columnIndex = columnIndex + 1,
+                                    direction = East,
+                                    stepsTaken = stepsTaken,
+                                ),
+                                cooled + map[rowIndex][columnIndex + 1],
+                            )
+                        )
                     }
                 }
-            }
 
-            fun goWest(stepsTaken: Int) {
-                if (columnIndex > 0) {
-                    val k = copy(
-                        columnIndex = columnIndex - 1,
-                        direction = West,
-                        stepsTaken = stepsTaken,
-                    )
-                    val c = cooled + map[rowIndex][columnIndex - 1]
-                    val q = visited[k]
-                    if (q == null || q > c) {
-                        visited[k] = c
-                        add(Pair(k, c))
+                fun goWest(stepsTaken: Int) {
+                    if (columnIndex > 0) {
+                        add(
+                            Pair(
+                                copy(
+                                    columnIndex = columnIndex - 1,
+                                    direction = West,
+                                    stepsTaken = stepsTaken,
+                                ),
+                                cooled + map[rowIndex][columnIndex - 1],
+                            )
+                        )
                     }
                 }
-            }
 
-            when (direction) {
-                North -> {
-                    goEast(1)
-                    goWest(1)
-                    if (stepsTaken < 3) goNorth(stepsTaken + 1)
-                }
+                when (direction) {
+                    North -> {
+                        goEast(1)
+                        goWest(1)
+                        if (stepsTaken < 3) goNorth(stepsTaken + 1)
+                    }
 
-                West -> {
-                    goNorth(1)
-                    goSouth(1)
-                    if (stepsTaken < 3) goWest(stepsTaken + 1)
-                }
+                    West -> {
+                        goNorth(1)
+                        goSouth(1)
+                        if (stepsTaken < 3) goWest(stepsTaken + 1)
+                    }
 
-                East -> {
-                    goNorth(1)
-                    goSouth(1)
-                    if (stepsTaken < 3) goEast(stepsTaken + 1)
-                }
+                    East -> {
+                        goNorth(1)
+                        goSouth(1)
+                        if (stepsTaken < 3) goEast(stepsTaken + 1)
+                    }
 
-                South -> {
-                    goEast(1)
-                    goWest(1)
-                    if (stepsTaken < 3) goSouth(stepsTaken + 1)
+                    South -> {
+                        goEast(1)
+                        goWest(1)
+                        if (stepsTaken < 3) goSouth(stepsTaken + 1)
+                    }
                 }
             }
         }
@@ -130,121 +135,119 @@ fun main() {
                 Crucible(rowIndex = 0, columnIndex = 0, direction = East, stepsTaken = 0),
                 Crucible(rowIndex = 0, columnIndex = 0, direction = South, stepsTaken = 0),
             )
-                .flatMap { it.nextStart(0) }
+                .flatMap { Pair(it, 0L).nextStart() }
                 .toSet(),
-            comparator = compareBy { (_, cooled) -> cooled },
-            isEnd = { first.rowIndex == map.size - 1 && first.columnIndex == map[0].size - 1 },
-            nextNodes = { first.nextStart(second) },
+            isEnd = { rowIndex == map.size - 1 && columnIndex == map[0].size - 1 },
+            nextNodes = { nextStart() },
         ).second
     }
 
     fun part2(input: List<String>): Long {
         val map = input.map { line -> line.map { it.code - '0'.code } }
-        val visited = mutableMapOf<Crucible, Long>()
 
-        fun Crucible.nextStart(cooled: Long): Set<Pair<Crucible, Long>> = buildSet {
-            fun goNorth(stepsTaken: Int) {
-                if (rowIndex > 0) {
-                    val k = copy(
-                        rowIndex = rowIndex - 1,
-                        direction = North,
-                        stepsTaken = stepsTaken,
-                    )
-                    val c = cooled + map[rowIndex - 1][columnIndex]
-                    val q = visited[k]
-                    if (q == null || q > c) {
-                        visited[k] = c
-                        add(Pair(k, c))
-                    }
-                }
-            }
+        fun Pair<Crucible, Long>.nextStart(): Set<Pair<Crucible, Long>> = buildSet {
+            val (crucible, cooled) = this@nextStart
 
-            fun goSouth(stepsTaken: Int) {
-                if (rowIndex < map.size - 1) {
-                    val k = copy(
-                        rowIndex = rowIndex + 1,
-                        direction = South,
-                        stepsTaken = stepsTaken,
-                    )
-                    val c = cooled + map[rowIndex + 1][columnIndex]
-                    val q = visited[k]
-                    if (q == null || q > c) {
-                        visited[k] = c
-                        add(Pair(k, c))
-                    }
-                }
-            }
-
-            fun goEast(stepsTaken: Int) {
-                if (columnIndex < map[0].size - 1) {
-                    val k = copy(
-                        columnIndex = columnIndex + 1,
-                        direction = East,
-                        stepsTaken = stepsTaken,
-                    )
-                    val c = cooled + map[rowIndex][columnIndex + 1]
-                    val q = visited[k]
-                    if (q == null || q > c) {
-                        visited[k] = c
-                        add(Pair(k, c))
-                    }
-                }
-            }
-
-            fun goWest(stepsTaken: Int) {
-                if (columnIndex > 0) {
-                    val k = copy(
-                        columnIndex = columnIndex - 1,
-                        direction = West,
-                        stepsTaken = stepsTaken,
-                    )
-                    val c = cooled + map[rowIndex][columnIndex - 1]
-                    val q = visited[k]
-                    if (q == null || q > c) {
-                        visited[k] = c
-                        add(Pair(k, c))
-                    }
-                }
-            }
-
-            when (direction) {
-                North -> {
-                    if (stepsTaken < 4) {
-                        goNorth(stepsTaken + 1)
-                    } else {
-                        goEast(1)
-                        goWest(1)
-                        if (stepsTaken < 10) goNorth(stepsTaken + 1)
+            with(crucible) {
+                fun goNorth(stepsTaken: Int) {
+                    if (rowIndex > 0) {
+                        add(
+                            Pair(
+                                copy(
+                                    rowIndex = rowIndex - 1,
+                                    direction = North,
+                                    stepsTaken = stepsTaken,
+                                ),
+                                cooled + map[rowIndex - 1][columnIndex],
+                            )
+                        )
                     }
                 }
 
-                West -> {
-                    if (stepsTaken < 4) {
-                        goWest(stepsTaken + 1)
-                    } else {
-                        goNorth(1)
-                        goSouth(1)
-                        if (stepsTaken < 10) goWest(stepsTaken + 1)
+                fun goSouth(stepsTaken: Int) {
+                    if (rowIndex < map.size - 1) {
+                        add(
+                            Pair(
+                                copy(
+                                    rowIndex = rowIndex + 1,
+                                    direction = South,
+                                    stepsTaken = stepsTaken,
+                                ),
+                                cooled + map[rowIndex + 1][columnIndex],
+                            )
+                        )
                     }
                 }
 
-                East -> {
-                    if (stepsTaken < 4) {
-                        goEast(stepsTaken + 1)
-                    } else {
-                        goNorth(1)
-                        goSouth(1)
-                        if (stepsTaken < 10) goEast(stepsTaken + 1)
+                fun goEast(stepsTaken: Int) {
+                    if (columnIndex < map[0].size - 1) {
+                        add(
+                            Pair(
+                                copy(
+                                    columnIndex = columnIndex + 1,
+                                    direction = East,
+                                    stepsTaken = stepsTaken,
+                                ),
+                                cooled + map[rowIndex][columnIndex + 1],
+                            )
+                        )
                     }
                 }
 
-                South -> {
-                    if (stepsTaken < 4) {
-                        goSouth(stepsTaken + 1)
-                    } else {
-                        goEast(1)
-                        goWest(1)
-                        if (stepsTaken < 10) goSouth(stepsTaken + 1)
+                fun goWest(stepsTaken: Int) {
+                    if (columnIndex > 0) {
+                        add(
+                            Pair(
+                                copy(
+                                    columnIndex = columnIndex - 1,
+                                    direction = West,
+                                    stepsTaken = stepsTaken,
+                                ),
+                                cooled + map[rowIndex][columnIndex - 1],
+                            )
+                        )
+                    }
+                }
+
+                when (direction) {
+                    North -> {
+                        if (stepsTaken < 4) {
+                            goNorth(stepsTaken + 1)
+                        } else {
+                            goEast(1)
+                            goWest(1)
+                            if (stepsTaken < 10) goNorth(stepsTaken + 1)
+                        }
+                    }
+
+                    West -> {
+                        if (stepsTaken < 4) {
+                            goWest(stepsTaken + 1)
+                        } else {
+                            goNorth(1)
+                            goSouth(1)
+                            if (stepsTaken < 10) goWest(stepsTaken + 1)
+                        }
+                    }
+
+                    East -> {
+                        if (stepsTaken < 4) {
+                            goEast(stepsTaken + 1)
+                        } else {
+                            goNorth(1)
+                            goSouth(1)
+                            if (stepsTaken < 10) goEast(stepsTaken + 1)
+                        }
+                    }
+
+                    South -> {
+                        if (stepsTaken < 4) {
+                            goSouth(stepsTaken + 1)
+                        } else {
+                            goEast(1)
+                            goWest(1)
+                            if (stepsTaken < 10) goSouth(stepsTaken + 1)
+                        }
                     }
                 }
             }
@@ -255,11 +258,10 @@ fun main() {
                 Crucible(rowIndex = 0, columnIndex = 0, direction = East, stepsTaken = 0),
                 Crucible(rowIndex = 0, columnIndex = 0, direction = South, stepsTaken = 0),
             )
-                .flatMap { it.nextStart(0) }
+                .flatMap { Pair(it, 0L).nextStart() }
                 .toSet(),
-            comparator = compareBy { (_, cooled) -> cooled },
-            isEnd = { first.rowIndex == map.size - 1 && first.columnIndex == map[0].size - 1 },
-            nextNodes = { first.nextStart(second) },
+            isEnd = { rowIndex == map.size - 1 && columnIndex == map[0].size - 1 },
+            nextNodes = { nextStart() },
         ).second
     }
 
